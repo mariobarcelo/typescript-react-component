81e048637979d368cbfb5a4022200a27a244a586
Mario Barceló <mariobarcelosospedra@gmail.com>
2023-07-05 12:10:05 +0200

    Imrove time ago display on mobile

--- src/components/modules/comment/CommentCard/CommentCard.tsx
+++ src/components/modules/comment/CommentCard/CommentCard.tsx
@@ -9,10 +9,10 @@ const CommentCard = ({ comment }) => {
         <div className="flex-shrink-0">
           <Avatar src={comment.userImage} alt={`Avatar of ${comment.user.fullName}`} /> 
         </div>
-        <div className="min-w-0 flex-1 grid grid-cols-[auto_1fr] grid-rows-[auto_auto] gap-x-3 gap-y-1">
+        <div className="min-w-0 flex-1 grid grid-cols-[auto_1fr] grid-rows-[auto_auto_auto] sm:grid-rows-[auto_auto] gap-x-3 gap-y-1">
           <p className='text-black text-left justify-self-start'>{comment.user.fullName}</p>
-          <span className='text-sm text-[#9197a1] justify-self-start self-center'>{`${formatDistanceToNow(comment.created)} ago`}</span>
-          <p className='text-black text-left col-start-1 col-end-3 max-w-[75ch]'>{comment.description}</p>
+          <span className='text-sm text-[#9197a1] row-start-2 sm:row-start-1 sm:col-start-2 justify-self-start self-center'>{`${formatDistanceToNow(comment.created)} ago`}</span>
+          <p className='text-black text-left pt-2 sm:pt-0 col-start-1 col-end-3 max-w-[75ch]'>{comment.description}</p>
         </div>
     </div>
   )
